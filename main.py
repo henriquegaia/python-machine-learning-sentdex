3e449628c7640f24002dde98ca77ece3145a6531
henrique moreira <henrique.gaia@gmail.com>
2017-02-03 20:31:53 +0000

    episodes 1-2

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import quandl
+
+df = quandl.get("WIKI/GOOGL")
+
+print df.head()
+
+df = df[[
+    'Adj. Open',
+    'Adj. High',
+    'Adj. Low',
+    'Adj. Close',
+    'Adj. Volume',
+]];
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100;
+
+df['PCT_CH'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100;
+
+df = df[[
+    'Adj. Close',
+    'HL_PCT',
+    'PCT_CH',
+    'Adj. Volume',
+]];
+
+# forecast_col = 'Adj. Close'
+
+# df.fillna(-99999, inplace = True)
+
+print df.head()
